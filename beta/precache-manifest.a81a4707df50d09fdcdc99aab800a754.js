81687c52be76ecfb05b15f9a9ecdda70a65152f8
mvermont <mvermont@udel.edu>
2020-02-27 13:41:29 -0500

    Updates

---
+++ beta/precache-manifest.a81a4707df50d09fdcdc99aab800a754.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fd0dcea47820063efe7dabd12e32b87",
+    "url": "/coding-2-try-phaser-mvermont/betaassets/background.png"
+  },
+  {
+    "revision": "6be3462bcaa4a183b678800bb66d176c",
+    "url": "/coding-2-try-phaser-mvermont/betaassets/beam.png"
+  },
+  {
+    "revision": "addb42da418cf47ecb86664c86f0b762",
+    "url": "/coding-2-try-phaser-mvermont/betaassets/citations.md"
+  },
+  {
+    "revision": "ea801e18a4f14b0c4740b555536b414e",
+    "url": "/coding-2-try-phaser-mvermont/betaassets/explosion.png"
+  },
+  {
+    "revision": "ff70e55fbb2b70941d98b3e7ab1a01aa",
+    "url": "/coding-2-try-phaser-mvermont/betaassets/player.png"
+  },
+  {
+    "revision": "05427110fd2c2de1d4738b31496a53c9",
+    "url": "/coding-2-try-phaser-mvermont/betaassets/power-up.png"
+  },
+  {
+    "revision": "6387e4bedb194c28efe573c1cc5ce303",
+    "url": "/coding-2-try-phaser-mvermont/betaassets/ship.png"
+  },
+  {
+    "revision": "4888982b102ce1a7c57fb9f8c129e63d",
+    "url": "/coding-2-try-phaser-mvermont/betaassets/ship2.png"
+  },
+  {
+    "revision": "15d30c88882683af9f5092c13dc13853",
+    "url": "/coding-2-try-phaser-mvermont/betaassets/ship3.png"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/coding-2-try-phaser-mvermont/betafavicon.ico"
+  },
+  {
+    "revision": "feff41587253f2cc18a4",
+    "url": "/coding-2-try-phaser-mvermont/betagen.main.d36b7da4fa646530f70c.bundle.js"
+  },
+  {
+    "revision": "7e3abc0967d71642756e",
+    "url": "/coding-2-try-phaser-mvermont/betagen.vendors.bbe9474c65e057812275.bundle.js"
+  },
+  {
+    "revision": "c2683483a2a66f8569314438283a3e30",
+    "url": "/coding-2-try-phaser-mvermont/betaindex.html"
+  },
+  {
+    "revision": "b4321e6f6a88a978d4964c0d1b432b03",
+    "url": "/coding-2-try-phaser-mvermont/betamanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/coding-2-try-phaser-mvermont/betasw.js"
+  }
+]);
