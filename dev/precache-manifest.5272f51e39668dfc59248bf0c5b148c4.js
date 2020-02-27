c5ec66b5ed5dd898f3e079fadfb7c4ff80810c26
mvermont <mvermont@udel.edu>
2020-02-26 19:30:04 -0500

    Updates

---
+++ dev/precache-manifest.5272f51e39668dfc59248bf0c5b148c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7087d5d91321d575b038366ef9f2407",
+    "url": "/coding-2-try-phaser-mvermont/devassets/citations.md"
+  },
+  {
+    "revision": "ecb0706068c64e384f8a",
+    "url": "/coding-2-try-phaser-mvermont/devgen.main.0303892dc13ad3108766.bundle.js"
+  },
+  {
+    "revision": "201b65bee9df6270f739",
+    "url": "/coding-2-try-phaser-mvermont/devgen.vendors.a047ee0740c7d21714d9.bundle.js"
+  },
+  {
+    "revision": "dddefa94592d05b5461b95db90497719",
+    "url": "/coding-2-try-phaser-mvermont/devindex.html"
+  },
+  {
+    "revision": "b4321e6f6a88a978d4964c0d1b432b03",
+    "url": "/coding-2-try-phaser-mvermont/devmanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/coding-2-try-phaser-mvermont/devsw.js"
+  }
+]);
--- dev/sw.js
+++ dev/sw.js
@@ -1,4 +1,4 @@
-importScripts("/educational-phaser-game-template/devprecache-manifest.549ef5a400021d602b1a18ffdc9f4103.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/coding-2-try-phaser-mvermont/devprecache-manifest.5272f51e39668dfc59248bf0c5b148c4.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /**
  * You should only modify this, if you know what you are doing.
