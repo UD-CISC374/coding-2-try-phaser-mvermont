1f69cb717a3999c73c7b755e1dc5f1825125e4ed
mvermont <mvermont@udel.edu>
2020-02-26 21:17:33 -0500

    Updates

---
+++ alpha/assets/citations.md
@@ -0,0 +1,6 @@
+*Please be sure to list the source of all assets you used here.*
+
+*At least a URL, but also be sure to include any extra information that the artist/author requests you include.*
+https://www.google.com/search?q=hell&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjpldTfw_DnAhVlS98KHYWPAHAQ_AUoAXoECA4QAw&biw=1536&bih=722#imgrc=5SIxQO_roRCbmM
+
+https://github.com/ansimuz/getting-started-with-phaser/tree/master/Part%203%20-%20Images%20Crazy%20Example/assets/images
---
+++ alpha/precache-manifest.b8900a3be9f97d7c5e05cef98c538f5f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f194de51ef51ed46848d9f4b90955a30",
+    "url": "/educational-phaser-game-template/alphaassets/background.jpg"
+  },
+  {
+    "revision": "11ce323077bcaf97c2e39ab356cb9c2d",
+    "url": "/educational-phaser-game-template/alphaassets/citations.md"
+  },
+  {
+    "revision": "ea801e18a4f14b0c4740b555536b414e",
+    "url": "/educational-phaser-game-template/alphaassets/explosion.png"
+  },
+  {
+    "revision": "6387e4bedb194c28efe573c1cc5ce303",
+    "url": "/educational-phaser-game-template/alphaassets/ship.png"
+  },
+  {
+    "revision": "4888982b102ce1a7c57fb9f8c129e63d",
+    "url": "/educational-phaser-game-template/alphaassets/ship2.png"
+  },
+  {
+    "revision": "15d30c88882683af9f5092c13dc13853",
+    "url": "/educational-phaser-game-template/alphaassets/ship3.png"
+  },
+  {
+    "revision": "4f4807b4405de79cee0b",
+    "url": "/educational-phaser-game-template/alphagen.main.dacc338b597071b25a5d.bundle.js"
+  },
+  {
+    "revision": "201b65bee9df6270f739",
+    "url": "/educational-phaser-game-template/alphagen.vendors.a047ee0740c7d21714d9.bundle.js"
+  },
+  {
+    "revision": "2fecc5fb538160a8469e6abfe28a3fa9",
+    "url": "/educational-phaser-game-template/alphaindex.html"
+  },
+  {
+    "revision": "b4321e6f6a88a978d4964c0d1b432b03",
+    "url": "/educational-phaser-game-template/alphamanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/educational-phaser-game-template/alphasw.js"
+  }
+]);
