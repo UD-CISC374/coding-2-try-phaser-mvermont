4484adcf5d4af63518563e5cf152402bdbefa4c8
mvermont <mvermont@udel.edu>
2020-02-28 13:26:23 -0500

    Updates

---
+++ final/precache-manifest.55bcf33ba9f254cfb121a0f588156cee.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8a7824ac7893230fe6b4007c09aae57",
+    "url": "/coding-2-try-phaser-mvermont/finalassets/background.jpg"
+  },
+  {
+    "revision": "6be3462bcaa4a183b678800bb66d176c",
+    "url": "/coding-2-try-phaser-mvermont/finalassets/beam.png"
+  },
+  {
+    "revision": "addb42da418cf47ecb86664c86f0b762",
+    "url": "/coding-2-try-phaser-mvermont/finalassets/citations.md"
+  },
+  {
+    "revision": "ea801e18a4f14b0c4740b555536b414e",
+    "url": "/coding-2-try-phaser-mvermont/finalassets/explosion.png"
+  },
+  {
+    "revision": "9d79a2ec01455afa082d23e42f457801",
+    "url": "/coding-2-try-phaser-mvermont/finalassets/fonts/font.png"
+  },
+  {
+    "revision": "872f41e77eae3bfc4fba291b0145b12a",
+    "url": "/coding-2-try-phaser-mvermont/finalassets/fonts/font.xml"
+  },
+  {
+    "revision": "ff70e55fbb2b70941d98b3e7ab1a01aa",
+    "url": "/coding-2-try-phaser-mvermont/finalassets/player.png"
+  },
+  {
+    "revision": "05427110fd2c2de1d4738b31496a53c9",
+    "url": "/coding-2-try-phaser-mvermont/finalassets/power-up.png"
+  },
+  {
+    "revision": "6387e4bedb194c28efe573c1cc5ce303",
+    "url": "/coding-2-try-phaser-mvermont/finalassets/ship.png"
+  },
+  {
+    "revision": "4888982b102ce1a7c57fb9f8c129e63d",
+    "url": "/coding-2-try-phaser-mvermont/finalassets/ship2.png"
+  },
+  {
+    "revision": "15d30c88882683af9f5092c13dc13853",
+    "url": "/coding-2-try-phaser-mvermont/finalassets/ship3.png"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/coding-2-try-phaser-mvermont/finalfavicon.ico"
+  },
+  {
+    "revision": "6969b15c37e44a9c6716",
+    "url": "/coding-2-try-phaser-mvermont/finalgen.main.b09a4774b174e4268a09.bundle.js"
+  },
+  {
+    "revision": "7e3abc0967d71642756e",
+    "url": "/coding-2-try-phaser-mvermont/finalgen.vendors.bbe9474c65e057812275.bundle.js"
+  },
+  {
+    "revision": "f3b1ff567a0c4b624e7965e2068a8ff5",
+    "url": "/coding-2-try-phaser-mvermont/finalicons/icons-192.png"
+  },
+  {
+    "revision": "940dadf1b7379fa02e546441b85f487a",
+    "url": "/coding-2-try-phaser-mvermont/finalicons/icons-512.png"
+  },
+  {
+    "revision": "9c4c0767cb281e1f5b7c0310db28f12c",
+    "url": "/coding-2-try-phaser-mvermont/finalindex.html"
+  },
+  {
+    "revision": "b4321e6f6a88a978d4964c0d1b432b03",
+    "url": "/coding-2-try-phaser-mvermont/finalmanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/coding-2-try-phaser-mvermont/finalsw.js"
+  }
+]);
